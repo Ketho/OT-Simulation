c15c383ad2e7bd3fa1dd491fe0f50f1cbd12960a
LennoINS <57046937+LennoINS@users.noreply.github.com>
2024-11-03 22:07:50 +0100

    Update entrypoint.sh

--- epanet/entrypoint.sh
+++ epanet/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Find any .inp file in the /app/epanet folder
-INP_FILE=$(find /app/epanet -name "*.inp" | head -n 1)
+# Find any .inp file in the /app folder
+INP_FILE=$(find /app -name "*.inp" | head -n 1)
 
 # Check if an .inp file was found
 if [ -z "$INP_FILE" ]; then
-    echo "No .inp file found in /app/epanet!"
+    echo "No .inp file found in /app"
     exit 1
 fi
 
 # Run the Python script with the found .inp file as argv2
-python /app/epanet/epanet.py "$INP_FILE"
+python /app/epanet.py "$INP_FILE"
