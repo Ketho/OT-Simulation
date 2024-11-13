13126a12eb9939a1b6da877792ad8fce7f908a65
Vincent Hasper <125301365+haspervincent@users.noreply.github.com>
2024-11-13 01:49:40 +0100

    Update epanet.py

--- epanet/app/epanet.py
+++ epanet/app/epanet.py
@@ -6,100 +6,45 @@ from pymodbus.client import ModbusTcpClient
 import sys
 import time
 
-def parse_arguments() -> str:
-    """Parse command line arguments to retrieve EPANET network file."""
-    if len(sys.argv) != 3 or sys.argv[1] != '--network':
-        print("Run EPANET simulation with Modbus controls.")
-        print(f">>> python {sys.argv[0]} --network [network.inp]")
-        sys.exit(1)
-    return sys.argv[2]
-
-def setup_client() -> ModbusTcpClient:
-    """Setup and establish a Modbus TCP client connection."""
+def main():
     try:
-        client = ModbusTcpClient(host='127.0.0.1', port=502)
+        client = ModbusTcpClient(host='openplc', port=502)
         while not client.connect(): time.sleep(1) # retrying every second until a successful connection is established.
-        return client
-    except Exception as e:
-        print(f"Failed to setup and establish a Modbus TCP client connection: {e}")
-        sys.exit(1)
 
-def setup_epanet(network_file: str) -> epanet:
-    """Setup EPANET with specified network (.inp) file."""
-    try:
-        en = epanet(network_file)
+        en = epanet('net1_tank_scenario.inp')
         en.setTimeSimulationDuration(10) # initial setup; duration will be set to infinite in main function.
         en.setTimeHydraulicStep(1)
-        return en
-    except Exception as e:
-        print(f"Failed to setup EPANET with specified network (.inp) file: {e}")
-        sys.exit(1)
-
-def get_controls(client: ModbusTcpClient) -> dict:
-    """Get controls from OpenPLC."""
-    try:
-        return {'pipe_statuses': client.read_coils(0, 100).bits, 'pump_settings': client.read_holding_registers(0, 100).registers}
-    except Exception as e:
-        print(f"Failed to get controls from OpenPLC: {e}")
-        sys.exit(1)
-
-def set_controls(en: epanet, controls: dict) -> None:
-    """Set OpenPLC controls to EPANET network."""
-    try:
-        for i, status in zip(en.getLinkPipeIndex(), controls['pipe_statuses']): en.setLinkStatus(i, status)
-        for i, setting in zip(en.getLinkPumpIndex(), controls['pump_settings']): en.setLinkSettings(i, setting) # ~setting & 1
-    except Exception as e:
-        print(f"Failed to set OpenPLC controls to EPANET network: {e}")
-        sys.exit(1)
-
-def read_data(en: epanet) -> dict:
-    """Read data from EPANET network."""
-    try:
-        return {'junction_pressures': [float(en.getNodePressure(i)) for i in en.getNodeJunctionIndex()], 'tank_heads': [float(en.getNodeHydraulicHead(i)) for i in en.getNodeTankIndex()], 'pump_flows': [float(en.getLinkFlows(i)) for i in en.getLinkPumpIndex()]}
-    except Exception as e:
-        print(f"Failed to read data from EPANET network: {e}")
-        sys.exit(1)
-
-def write_data(client: ModbusTcpClient, data: dict) -> None:
-    """Write EPANET network data to OpenPLC."""
-    try:
-        for i, value in enumerate(data['junction_pressures']):
-            registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
-            client.write_registers(100 + i * 2, registers)
-
-        for i, value in enumerate(data['tank_heads']):
-            registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
-            client.write_registers(200 + i * 2, registers)
-        
-        for i, value in enumerate(data['pump_flows']):
-            registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
-            client.write_registers(300 + i * 2, registers)
-    except Exception as e:
-        print(f"Failed to write EPANET network data to OpenPLC: {e}")
-        sys.exit(1)
-
-def main():
-    network_file = parse_arguments()
-
-    try:
-        client = setup_client()
-        en = setup_epanet(network_file)
 
         en.openHydraulicAnalysis()
         en.initializeHydraulicAnalysis()
-        
+
         while True:
             en.setTimeSimulationDuration(en.getTimeSimulationDuration() + en.getTimeHydraulicStep()) # this way the duration is set to infinite.
 
-            controls = get_controls(client)
-            set_controls(en, controls)
+            # Get controls from OpenPLC.
+            controls = {'pipe_statuses': client.read_coils(0, 100).bits, 'pump_settings': [i / 100.0 for i in client.read_holding_registers(0, 100).registers]}
+
+            # Set OpenPLC controls to EPANET network.
+            for i, status in zip(en.getLinkPipeIndex(), controls['pipe_statuses']): en.setLinkStatus(i, status)
+            for i, setting in zip(en.getLinkPumpIndex(), controls['pump_settings']): en.setLinkSettings(i, setting)
 
             en.runHydraulicAnalysis()
 
-            data = read_data(en)
-            write_data(client, data)
+            # Read data from EPANET network.
+            data = {'junction_pressures': [float(en.getNodePressure(i)) for i in en.getNodeJunctionIndex()], 'tank_heads': [float(en.getNodeHydraulicHead(i)) for i in en.getNodeTankIndex()], 'pump_flows': [float(en.getLinkFlows(i)) for i in en.getLinkPumpIndex()]}
+            
+            # Write EPANET network data to OpenPLC.
+            for i, value in enumerate(data['junction_pressures']):
+                registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
+                client.write_registers(100 + i * 2, registers)
 
-            print(data)
+            for i, value in enumerate(data['tank_heads']):
+                registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
+                client.write_registers(200 + i * 2, registers)
+            
+            for i, value in enumerate(data['pump_flows']):
+                registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
+                client.write_registers(300 + i * 2, registers)
 
             en.nextHydraulicAnalysisStep()
 
@@ -108,7 +53,7 @@ def main():
         print(">--- Program interrupted by user ---")
         sys.exit(0) # clean exit confirmed by user action.
     except Exception as e:
-        print(f"Failed to run the program due to an unexpected error: {e}")
+        print(f"Failed to run EPANET simulation due to an unexpected error: {e}")
         sys.exit(1)
     finally:
         if 'client' in locals():
