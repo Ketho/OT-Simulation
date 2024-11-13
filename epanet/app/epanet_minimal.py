a5347d4e93e43fd898e1e51fabfae94528de3b3f
Vincent Hasper <125301365+haspervincent@users.noreply.github.com>
2024-11-13 01:49:05 +0100

    Update epanet_minimal.py

--- epanet/app/epanet_minimal.py
+++ epanet/app/epanet_minimal.py
@@ -22,19 +22,17 @@ def main():
             en.setTimeSimulationDuration(en.getTimeSimulationDuration() + en.getTimeHydraulicStep()) # this way the duration is set to infinite.
 
             # Get controls from OpenPLC.
-            controls = {'pipe_statuses': client.read_coils(0, 100).bits, 'pump_settings': client.read_holding_registers(0, 100).registers}
+            controls = {'pipe_statuses': client.read_coils(0, 100).bits, 'pump_settings': [i / 100.0 for i in client.read_holding_registers(0, 100).registers]}
 
             # Set OpenPLC controls to EPANET network.
             for i, status in zip(en.getLinkPipeIndex(), controls['pipe_statuses']): en.setLinkStatus(i, status)
-            for i, setting in zip(en.getLinkPumpIndex(), controls['pump_settings']): en.setLinkSettings(i, setting) # ~setting & 1
+            for i, setting in zip(en.getLinkPumpIndex(), controls['pump_settings']): en.setLinkSettings(i, setting)
 
             en.runHydraulicAnalysis()
 
             # Read data from EPANET network.
             data = {'junction_pressures': [float(en.getNodePressure(i)) for i in en.getNodeJunctionIndex()], 'tank_heads': [float(en.getNodeHydraulicHead(i)) for i in en.getNodeTankIndex()], 'pump_flows': [float(en.getLinkFlows(i)) for i in en.getLinkPumpIndex()]}
             
-            print(data)
-
             # Write EPANET network data to OpenPLC.
             for i, value in enumerate(data['junction_pressures']):
                 registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
@@ -55,7 +53,7 @@ def main():
         print(">--- Program interrupted by user ---")
         sys.exit(0) # clean exit confirmed by user action.
     except Exception as e:
-        print(f"Failed to run the program due to an unexpected error: {e}")
+        print(f"Failed to run EPANET simulation due to an unexpected error: {e}")
         sys.exit(1)
     finally:
         if 'client' in locals():
