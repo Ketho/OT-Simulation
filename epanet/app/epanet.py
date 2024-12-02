aff21e2efc6c302711d587c39d437215fa40290b
LennoINS <57046937+LennoINS@users.noreply.github.com>
2024-12-02 18:04:29 +0100

    Update epanet.py

--- epanet/app/epanet.py
+++ epanet/app/epanet.py
@@ -10,21 +10,6 @@ from pymodbus.client import ModbusTcpClient
 import sys
 import time
 
-def print_overview(en: epanet) -> None:
-    """Print overview of total number nodes and links in EPANET network."""
-    try:
-        print("Network Overview:")
-        print("-" * 32)
-        print(f" junctions : {en.getNodeJunctionCount()}")
-        print(f"reservoirs : {en.getNodeReservoirCount()}")
-        print(f"     tanks : {en.getNodeTankCount()}")
-        print(f"     pipes : {en.getLinkPipeCount()}")
-        print(f"     pumps : {en.getLinkPumpCount()}")
-        print(f"    valves : {en.getLinkValveCount()}")
-    except Exception as e:
-        print(f"Failed to print overview of total number nodes and links in EPANET network: {e}")
-        sys.exit(1)
-
 def parse_arguments() -> str:
     """Parse command line arguments to retrieve EPANET network file."""
     if len(sys.argv) != 2 or not sys.argv[1].endswith('.inp'):
@@ -55,57 +40,39 @@ def setup_epanet(network_file: str) -> epanet:
         print(f"Failed to setup EPANET with specified network (.inp) file: {e}")
         sys.exit(1)
 
-def get_controls(client: ModbusTcpClient) -> dict:
-    """Get controls from OpenPLC."""
-    try:
-        return {
-            'pipe_statuses': [i ^ 1 for i in client.read_coils(0, 100).bits], # using bitwise XOR (^) operator to set default pipe status to 1 (open).
-            'pump_settings': [i / 100.0 for i in client.read_holding_registers(0, 100).registers] # convert incoming integers to float values for ease of use (e.g. 100 = 1.0), as default pump setting is 1.0.
-        }
-    except Exception as e:
-        print(f"Failed to get controls from OpenPLC: {e}")
-        sys.exit(1)
-
-def set_controls(en: epanet, controls: dict) -> None:
-    """Set OpenPLC controls to EPANET network."""
-    try:
-        for i, status in zip(en.getLinkPipeIndex(), controls['pipe_statuses']): 
-            en.setLinkStatus(i, status)
-        for i, setting in zip(en.getLinkPumpIndex(), controls['pump_settings']): 
-            en.setLinkSettings(i, setting)
-    except Exception as e:
-        print(f"Failed to set OpenPLC controls to EPANET network: {e}")
-        sys.exit(1)
-
 def read_data(en: epanet) -> dict:
-    """Read data from EPANET network."""
-    try:
-        return {
-            'junction_pressures': [float(en.getNodePressure(i)) for i in en.getNodeJunctionIndex()], 
-            'tank_heads': [float(en.getNodeHydraulicHead(i)) for i in en.getNodeTankIndex()], 
-            'pump_flows': [float(en.getLinkFlows(i)) for i in en.getLinkPumpIndex()]
-        }
-    except Exception as e:
-        print(f"Failed to read data from EPANET network: {e}")
-        sys.exit(1)
+    """Gathers data from an EPANET network and categorizes it into zones.
 
-def write_data(client: ModbusTcpClient, data: dict) -> None:
-    """Write EPANET network data to OpenPLC."""
-    try:
-        for i, value in enumerate(data['junction_pressures']):
-            registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
-            client.write_registers(100 + i * 2, registers)
+    As this stupid shit has cost me to much fk time, this piece of art deserves to be treated with care.
+    """
+    data = {}
+    
+    for i in en.getNodeNameID() + en.getLinkNameID():
+        if '-' not in i:
+            continue
 
-        for i, value in enumerate(data['tank_heads']):
-            registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
-            client.write_registers(200 + i * 2, registers)
+        zone, e = i.split('-', 1)
+
+        if zone not in data:
+            data[zone] = {}
+        if e not in data[zone]:
+            data[zone][e] = {}
         
-        for i, value in enumerate(data['pump_flows']):
-            registers = client.convert_to_registers(value, client.DATATYPE.FLOAT32)
-            client.write_registers(300 + i * 2, registers)
-    except Exception as e:
-        print(f"Failed to write EPANET network data to OpenPLC: {e}")
-        sys.exit(1)
+        if i in en.getNodeNameID():
+            data[zone][e]['hydraulic_head'] = \
+                en.getNodeHydraulicHead(en.getNodeIndex(i))
+            
+            data[zone][e]['min_water_level'] = \
+                float(en.getNodeTankMinimumWaterLevel(en.getNodeIndex(i)))
+            
+            data[zone][e]['max_water_level'] = \
+                float(en.getNodeTankMaximumWaterLevel(en.getNodeIndex(i)))
+        
+        if i in en.getLinkNameID():
+            data[zone][e]['pump_power'] = \
+                en.getLinkPumpPower(en.getLinkIndex(i))
+            
+    return data
 
 def main():
     network_file = parse_arguments()
@@ -113,22 +80,23 @@ def main():
     try:
         client = setup_client()
         en = setup_epanet(network_file)
-    
-        print_overview(en)
 
         en.openHydraulicAnalysis()
         en.initializeHydraulicAnalysis()
         
         while True:
             en.setTimeSimulationDuration(en.getTimeSimulationDuration() + en.getTimeHydraulicStep()) # this way the duration is set to infinite.
-
-            controls = get_controls(client)
-            set_controls(en, controls)
+            
+            # controls = get_controls(client) here <---
+            # set_controls(en, controls) here <---
 
             en.runHydraulicAnalysis()
 
             data = read_data(en)
-            write_data(client, data)
+            # write_data(client, data) here <---
+            
+            # print(data)
+            import json; print(json.dumps(data, indent=4))
 
             en.nextHydraulicAnalysisStep()
 
@@ -148,4 +116,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
