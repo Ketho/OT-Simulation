cf3c6d82b5d6e6bff667d729418a45790af2cb66
LennoINS <57046937+LennoINS@users.noreply.github.com>
2024-12-02 17:35:29 +0100

    Update database.sh

--- openplc/database.sh
+++ openplc/database.sh
@@ -18,17 +18,17 @@ sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_REMOVE_DEVICE"
 SQL_SLAVE_Dev_1="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('Slave Name', 'TCP', 0, '192.168.1.1', 502, 0, 80, 0, 80, 0, 80, 0, 80, 0, 80);"
 sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_SLAVE_Dev_1"
 
+# Slave device 2
+SQL_SLAVE_Dev_2="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('Slave Name 2', 'TCP', 1, '192.168.1.2', 502, 0, 80, 0, 80, 0, 80, 0, 80, 0, 80);"
+sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_SLAVE_Dev_2"
+
+# Slave device 3
+SQL_SLAVE_Dev_3="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('Slave Name 3', 'TCP', 2, '192.168.1.3', 502, 0, 80, 0, 80, 0, 80, 0, 80, 0, 80);"
+sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_SLAVE_Dev_3"
+
 # Change or disable Modbus port. Comment out if not requried.
 SQL_UPDATE_Modbus="UPDATE Settings SET Value = '502' WHERE Key = 'Modbus_port';"
 sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_UPDATE_Modbus"
-
-# Change or disable DNP3 port. Comment out if not requried.
-SQL_UPDATE_DNP3="UPDATE Settings SET Value = '20000' WHERE Key = 'Dnp3_port';"
-sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_UPDATE_DNP3"
-
-# Change or disable ENIP port. Comment out if not requried.
-SQL_UPDATE_ENIP="UPDATE Settings SET Value = '44818' WHERE Key = 'ENIP_port';"
-sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_UPDATE_ENIP"
 
 # enable openplc start run mode. Comment out if not requried.
 SQL_Start_run_mode="UPDATE Settings SET Value = 'true' WHERE Key = 'Start_run_mode';"
